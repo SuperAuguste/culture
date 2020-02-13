25871ec0425affb2a97ba1cb11c38b333c8ec98a
SuperAuguste <auguste.rame@gmail.com>
2020-02-13 18:27:32 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Culture
+
+I made this for my MYP school project. Enjoy the deadline spaghetti.
---
+++ js/app.js
@@ -0,0 +1,58 @@
+const play = "▶";
+const pause = "❚❚";
+
+const tracked = new Set();
+
+document.body.addEventListener("click", event => {
+
+	const target = event.target;
+
+	if (!target) return;
+
+	if (target.classList.contains("play")) {
+
+		if (!tracked.has(target)) {
+			
+			tracked.add(target);
+			target.parentElement.querySelector("audio").addEventListener("timeupdate", () => {
+
+				target.parentElement.querySelector(".scrub").style.setProperty("--progress", `${target.parentElement.querySelector("audio").currentTime / target.parentElement.querySelector("audio").duration * 100}%`);
+
+			});
+			target.parentElement.querySelector("audio").addEventListener("play", () => {target.innerText = pause});
+			target.parentElement.querySelector("audio").addEventListener("pause", () => {target.innerText = play});
+
+		}
+
+		const playing = target.innerText === pause;
+
+		if (playing) {
+			
+			// Pause
+
+			target.innerText = play;
+			target.parentElement.querySelector("audio").pause();
+		
+		} else {
+
+			// Play
+			
+			target.innerText = pause;
+			target.parentElement.querySelector("audio").play();
+
+		}
+
+	} else if (target.classList.contains("scrub")) {
+
+		target.parentElement.querySelector("audio").currentTime = target.parentElement.querySelector("audio").duration * event.offsetX / target.offsetWidth;
+		target.style.setProperty("--progress", `${event.offsetX / target.offsetWidth * 100}%`);
+
+	} else if (target.classList.contains("progress")) {
+
+		const t = target.parentElement;
+		t.parentElement.querySelector("audio").currentTime = t.parentElement.querySelector("audio").duration * event.offsetX / t.offsetWidth;
+		t.style.setProperty("--progress", `${event.offsetX / t.offsetWidth * 100}%`);
+
+	}
+
+});
